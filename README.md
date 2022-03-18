496cfc0e214ee3cdf006d838a33453b6ae778b13
Ape Toshi <admin@apetoshi.com>
2022-03-18 12:50:49 +0100

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # discord-role-adder
 Automates the process of mass assigning roles.
+
+# Requirements
+Python >=3.7.0
+
+# Setup
+It's always a good idea to have a separate environment for each project.
+However, I'm feeling a bit lazy (again) so I'll show how to install packages globally
+
+```bash
+pip3 install -r requirements.txt
+```
+
+Add a `.env` file with the `DISCORD_TOKEN=<your_token>"` line. In this example I am using a `list.csv` obtained from ![premint](https://premint.xyz).
+In the `bot.py` file change the `ROLE_ID` to the role you will be assigning. You can use `\@rolename` in the chat to obtain it.
+
+Finally, run it with:
+
+```bash
+python3 bot.py
+```
+
+# Troubleshooting
+If you are gettin an error about missing certificates while connecting to the Discord host, run `install_certificate.py` to fix it.
---
+++ bot.py
@@ -0,0 +1,44 @@
+# bot.py
+import os
+import pandas as pd
+from discord.utils import get
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+ROLE_ID = 943555622148915291
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+    # for guild in client.guilds:
+    #     for member in guild.members:
+    #         print("Member:", member)
+    #         return
+
+    df = pd.read_csv("list.csv")
+
+    # for id in df["discord id"]:
+    #     print("Discord id:", id)
+
+    guild = client.guilds[0]
+    role = get(guild.roles, id=ROLE_ID)
+    print("Assigning role:", role)
+
+    users = await guild.query_members(user_ids=list(df["discord id"]))
+
+    for user in users:
+        try:
+            await user.add_roles(role)
+            print("[ADDED]:", user)
+        except Exception as e:
+            print("[EXCEPTION]:", e)
+
+client.run(TOKEN)
